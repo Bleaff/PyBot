bb1b369ff8510c52a4f0f9c66938309737749ebe
Bleaff <sergey.sysoev3010@gmail.com>
2022-09-09 00:32:29 +0300

    Start lines

---
+++ PythonTeleBot/botv1.py
@@ -0,0 +1,15 @@
+import telebot
+from telebot import types
+
+myBot = telebot.TeleBot("5319467602:AAHt1plxDpMvmPlwKvjqqqlpPZ8qfDju6iE")
+
+@myBot.message_handler(commands=['start'])
+def show_start_menu(message):
+	markup = types.ReplyKeyboardMarkup()
+	btn_start = types.KeyboardButton("Start timer")
+	btn_setup = types.KeyboardButton("Set Up")
+	markup.add(btn_start,btn_setup)
+
+@myBot.message_handler(content_types="text")
+def operate_commands(message):
+	pass
